838b17cbd728c639a7077749d2377b43fcfc0a9a
Karol <karolmarciniak790@gmail.com>
2025-06-06 10:38:25 +0000

    Initial commit

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["http:localhost:8000"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
+@app.get("/")
+async def root():
+	return  {"message": "Welcome to FastAPI!"}
