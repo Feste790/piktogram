c96baaed9479318507b7d8b1a23479ea24ea674b
Karol <karolmarciniak790@gmail.com>
2025-06-29 20:12:15 +0200

    add camera

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+import cv2
+import numpy as np
 
 app = FastAPI()
 
@@ -15,3 +18,13 @@ app.add_middleware(
 @app.get("/api")
 async def read_root():
     return {"test":"From API"}
+
+@app.get("/stream")
+async def stream():
+    response = requests.get("http://192.168.18.102:5000/video", stream=True)
+    return Response(content=response.raw, media_type="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
